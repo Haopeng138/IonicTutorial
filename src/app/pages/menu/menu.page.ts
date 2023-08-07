1c51d167acf2c7819541b2b2854a414121b869a1
haopeng <haopeng138@gmail.com>
2023-08-07 13:31:51 +0200

    dropdown menu & hide menu from different size

--- src/app/pages/menu/menu.page.ts
+++ src/app/pages/menu/menu.page.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { MenuController } from '@ionic/angular';
+import { Component, Host, HostListener, OnInit } from '@angular/core';
+import { MenuController, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-menu',
@@ -25,13 +25,30 @@ export class MenuPage implements OnInit {
       path: '/about'
     }
   ]
-  constructor(private menuCtrl: MenuController) { }
+
+  title = "Home"
+  constructor(private menuCtrl: MenuController, private plt: Platform) { }
 
   ngOnInit() {
+    const width = this.plt.width()
+    this.toggleMenu(width)
   }
 
   setTitle(title: string) {
-    console.log(title);
+    this.title = title
   }
 
+  toggleMenu(width: number) {
+    if (width > 768) {
+      this.menuCtrl.enable(false, 'myMenu')
+    } else {  
+      this.menuCtrl.enable(true, 'myMenu')
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  private onResize(event: any) {
+    const newWidth = event.target.innerWidth;
+    this.toggleMenu(newWidth)
+  }
 }
